c4da062e8a4dee53e52d5756dabe2a2496ecd68c
martinli0822 <martinli0822@gmai.com>
2014-05-12 19:19:28 +0800

    Feat头文件

--- ImageFeature/Features.h
+++ ImageFeature/Features.h
@@ -6,15 +6,44 @@
 
 //include
 #include "stdafx.h"
+#include <fstream>
 #include<cv.h>
 using namespace cv;
 using namespace std;
 
+#define GLCM 1
+#define EH 2
+#define SIFT 3
+#define GLCM_LEVEL 8
+
 class ImageFeature
 {
 	int id;
-	double *f1;
-	double *f2;
-	double *f3;
-	double distance(const ImageFeature& a, int id);
+	double* GrayLevelCoocurrenceMatrix;
+	double* EdgeHist;
+	double* Sift;
+	int GLCM_length;
+	int EH_length;
+	int SIFT_length;
+public:
+	double* getGLCM();
+	double* getEH();
+	double* getSIFT();
+	int getlength(int FeatID);
+	double Distance(ImageFeature a, int FeatID);
+	//Mat ReadImage(int _id);
 };
+
+class calculateFeature
+{
+	double* calcGLCM(Mat img, int offset1, int offset2);
+	double* calcEH(Mat img);
+	double* calcSIFT(Mat img);
+};
+
+	/*
+#define WIDTH 32
+#define HEIGHT 32
+#define CHANNEL 3
+#define IMAGESIZE 3073 // 32*32*3+1
+	*/
