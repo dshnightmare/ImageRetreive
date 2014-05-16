124e994ad9c33d9f9fe8e95e41139148b3263e17
dengsh <dshnightmare@gmail.com>
2014-05-16 17:42:37 +0800

    基本界面完成，能够载入图片库并分类别显示

--- Interface/InterfaceDlg.cpp
+++ Interface/InterfaceDlg.cpp
@@ -6,11 +6,11 @@
 #include "Interface.h"
 #include "InterfaceDlg.h"
 #include "afxdialogex.h"
+#include "CvvImage.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
-
 
 // CAboutDlg dialog used for App About
 
@@ -49,8 +49,23 @@ END_MESSAGE_MAP()
 
 CInterfaceDlg::CInterfaceDlg(CWnd* pParent /*=NULL*/)
 	: CDialogEx(CInterfaceDlg::IDD, pParent)
+	, queryImg(NULL)
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+	pLibImagesType = NULL;
+	queryImg = NULL;
+	imgs = NULL;
+}
+
+CInterfaceDlg::~CInterfaceDlg()
+{
+	if(queryImg)
+		delete queryImg;
+	if(imgs)
+		delete[] imgs;
+	for(int i = 0; i < SHOWIMGROW * SHOWIMGCOL; i++)
+		if(pLibImages[i])
+			delete pLibImages[i];
 }
 
 void CInterfaceDlg::DoDataExchange(CDataExchange* pDX)
@@ -62,6 +77,11 @@ BEGIN_MESSAGE_MAP(CInterfaceDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_LOAD, &CInterfaceDlg::OnBnClickedLoad)
+	ON_CONTROL_RANGE(STN_CLICKED, IDC_IMGLIB, IDC_IMGLIB+39, &CInterfaceDlg::OnImgClickedLib)
+	//ON_STN_CLICKED(IDC_IMGLIB, &CInterfaceDlg::OnImgClickedLib)
+	ON_BN_CLICKED(IDC_GO, &CInterfaceDlg::OnBnClickedGo)
+	ON_CBN_SELCHANGE(IDC_TYPE, &CInterfaceDlg::OnCbnSelChage)
 END_MESSAGE_MAP()
 
 
@@ -97,6 +117,47 @@ BOOL CInterfaceDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// Set small icon
 
 	// TODO: Add extra initialization here
+	// initial comboBox
+	pLibImagesType = (CComboBox *)GetDlgItem(IDC_TYPE);
+	pLibImagesType->InsertString(0, L"airplane");
+	pLibImagesType->InsertString(1, L"automobile");
+	pLibImagesType->InsertString(2, L"bird");
+	pLibImagesType->InsertString(3, L"cat");
+	pLibImagesType->InsertString(4, L"deer");
+	pLibImagesType->InsertString(5, L"dog");
+	pLibImagesType->InsertString(6, L"frog");
+	pLibImagesType->InsertString(7, L"horse");
+	pLibImagesType->InsertString(8, L"ship");
+	pLibImagesType->InsertString(9, L"truck");
+	pLibImagesType->InsertString(10, L"ALL");
+
+	// initial libGroup
+	CRect lib_rect;
+	GetDlgItem(IDC_LIBGROUP)->GetWindowRect(&lib_rect);
+	int width = (lib_rect.Width() - 4) / SHOWIMGCOL - 2;
+	int height = (lib_rect.Height() - 20)/ SHOWIMGROW - 2;
+	int x = lib_rect.left - 70;
+	int y = lib_rect.top - 15;
+	for(int i = 0; i < SHOWIMGROW; i++)
+	{
+		x = lib_rect.left - 70;
+		for(int j = 0; j < SHOWIMGCOL; j++)
+		{
+			int num = i * SHOWIMGCOL + j;
+			pLibImages[num] = new CStatic;
+			CRect rect(x, y, x + width, y + height);
+			pLibImages[num]->Create(NULL, WS_CHILD | WS_VISIBLE | SS_NOTIFY, 
+				rect, this, IDC_IMGLIB + num);
+			//pLibImages[num]->SetParent(this);
+			x += (width + 2);
+		}
+		y += (height + 2);
+	}
+	// initial queryImg
+	CvSize imgSize;
+	imgSize.width = IMAGE_WIDTH;
+	imgSize.height = IMAGE_HEIGHT;
+	queryImg = cvCreateImage(imgSize, IPL_DEPTH_8U, IMAGE_CHANNEL);
 
 	return TRUE;  // return TRUE  unless you set the focus to a control
 }
@@ -140,6 +201,8 @@ void CInterfaceDlg::OnPaint()
 	else
 	{
 		CDialogEx::OnPaint();
+		/*CDialogEx::UpdateWindow();
+		ShowImage(queryImg, IDC_queryImg);*/
 	}
 }
 
@@ -150,3 +213,140 @@ HCURSOR CInterfaceDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+BOOL CInterfaceDlg:: LoadQRBinaryDataDll()
+{
+	BOOL bsuccess = TRUE;
+	m_hLoadImageLib = LoadLibrary(_T("Tools.dll"));
+	if (m_hLoadImageLib == NULL)
+	{
+		bsuccess = FALSE;
+	}
+	else
+	{
+		m_pfnLoadFromCIFAR10=
+			(PLoadFromCIFAR10)GetProcAddress(m_hLoadImageLib,"LoadFromCIFAR10");
+		bsuccess = TRUE;
+	}
+	return bsuccess;
+}
+
+
+void CInterfaceDlg::ShowImage(IplImage *img, CWnd *p, UINT id)
+{
+	CDC* pDC = p->GetDlgItem(id)->GetDC();
+	HDC hDC = pDC->GetSafeHdc();
+
+	CRect rect;
+	p->GetDlgItem(id)->GetClientRect(&rect);
+	// 求出图片控件的宽和高   
+	int rw = rect.right - rect.left;        
+	int rh = rect.bottom - rect.top;  
+	// 读取图片的宽和高  
+	int iw = img->width;    
+	int ih = img->height;   
+	// 使图片的显示位置正好在控件的正中   
+	int tx = (int)(rw - iw)/2;   
+	int ty = (int)(rh - ih)/2;   
+	SetRect(rect, tx, ty, tx+iw, ty+ih);
+
+	CvvImage cimg;
+	cimg.CopyOf(img);
+	cimg.DrawToHDC(hDC, &rect);
+	p->ReleaseDC(pDC);
+
+}
+
+
+void CInterfaceDlg::OnBnClickedLoad()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	if (!LoadQRBinaryDataDll())
+	{
+		MessageBox(L"error", L"DLL load error!", MB_OK);
+		return;
+	}
+	
+	imgs = (*m_pfnLoadFromCIFAR10)("G:\\Users File\\Documents\\Visual Studio 2012\\Projects\\cbir\\cbir\\data_batch_5.bin");
+	if(pLibImagesType)
+		pLibImagesType->SetCurSel(10);
+	for(int i = 0; i < SHOWIMGROW; i++)
+	{
+		for(int j = 0; j < SHOWIMGCOL; j++)
+		{
+			int num = i * SHOWIMGCOL + j;
+			Mat tmp = imgs[num].clone();
+			IplImage xx = tmp;
+			ShowImage(&xx, this, IDC_IMGLIB + num);
+			//ShowImage(&xx, this, IDC_IMGLIB + num);
+		}
+	}
+}
+
+void CInterfaceDlg::OnImgClickedLib(UINT nid)
+{
+	if(imgs && MessageBox(L"是否将该图片设置为查询图片？", L"设置查询图片", MB_YESNO) == IDYES)
+	{
+		*queryImg = imgs[nid - 2000];
+		//ResizeImage();
+		ShowImage(queryImg, this, IDC_queryImg);            // 调用显示图片函数
+	}
+}
+
+void CInterfaceDlg::OnCbnSelChage()
+{
+	char type = pLibImagesType->GetCurSel();
+	int n = -1;
+	if(NULL == imgs)
+		return;
+	for(int i = 0; i < SHOWIMGROW; i++)
+	{
+		for(int j = 0; j < SHOWIMGCOL; j++)
+		{
+			int num = i * SHOWIMGCOL + j;
+			while(imgs[++n].type != type)
+			{
+				if(type == 10)
+					break;
+			}
+			if(n >= 9999)
+				return;
+			Mat tmp = imgs[n].clone();
+			IplImage xx = tmp;
+			ShowImage(&xx, this, IDC_IMGLIB + num);
+			//ShowImage(&xx, this, IDC_IMGLIB + num);
+		}
+	}
+}
+void CInterfaceDlg::ResizeImage(IplImage * img)
+{
+	int w = img->width;
+	int h = img->height;
+
+	int max = (w > h)? w: h;
+
+	// 计算将图片缩放到TheImage区域所需的比例因子
+	float scale = (float) ( (float) max / 256.0f );
+
+	// 缩放后图片的宽和高
+	int nw = (int)( w/scale );
+	int nh = (int)( h/scale );
+
+	// 为了将缩放后的图片存入 TheImage 的正中部位，需计算图片在 TheImage 左上角的期望坐标值
+	int tlx = (nw > nh)? 0: (int)(256-nw)/2;
+	int tly = (nw > nh)? (int)(256-nh)/2: 0;
+
+	// 设置 TheImage 的 ROI 区域，用来存入图片 img
+	cvSetImageROI(queryImg, cvRect( tlx, tly, nw, nh) );
+
+	// 对图片 img 进行缩放，并存入到 TheImage 中
+	cvResize(img, queryImg);
+
+	// 重置 TheImage 的 ROI 准备读入下一幅图片
+	cvResetImageROI(queryImg);
+}
+
+void CInterfaceDlg::OnBnClickedGo()
+{
+	// TODO: 在此添加控件通知处理程序代码
+}
--- Interface/InterfaceDlg.h
+++ Interface/InterfaceDlg.h
@@ -1,9 +1,17 @@
 
 // InterfaceDlg.h : header file
 //
-
+#include "../Tools/LoadImageLib.h"
+#include <cv.h>
+#include <highgui.h>
 #pragma once
 
+
+#define IMAGE_WIDTH 32
+#define IMAGE_HEIGHT 32
+#define IMAGE_CHANNEL 3
+#define  SHOWIMGROW 4
+#define  SHOWIMGCOL 10
 
 // CInterfaceDlg dialog
 class CInterfaceDlg : public CDialogEx
@@ -11,11 +19,12 @@ class CInterfaceDlg : public CDialogEx
 // Construction
 public:
 	CInterfaceDlg(CWnd* pParent = NULL);	// standard constructor
+	~CInterfaceDlg();
 
 // Dialog Data
 	enum { IDD = IDD_INTERFACE_DIALOG };
 
-	protected:
+protected:
 	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
 
 
@@ -29,4 +38,45 @@ protected:
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+
+private:
+	//LoadImageLib
+	HINSTANCE m_hLoadImageLib;   //DLL¾ä±ú
+	PLoadFromCIFAR10 m_pfnLoadFromCIFAR10;
+	BOOL  LoadQRBinaryDataDll();
+
+	//¿Ø¼þ
+	CStatic *pLibImages[SHOWIMGCOL*SHOWIMGROW];
+	CStatic *pRstImages[SHOWIMGCOL*SHOWIMGROW];
+	CComboBox *pLibImagesType;
+
+	//²éÑ¯Í¼Æ¬
+	IplImage *queryImg;
+	
+	//Í¼Æ¬¿âÊý¾Ý
+	MyMat* imgs;
+
+	void ShowImage(IplImage * img, CWnd *p, UINT id);
+public:
+	afx_msg void OnBnClickedLoad();
+	afx_msg void OnImgClickedLib(UINT nid);
+	afx_msg void OnCbnSelChage();
+private:
+	void ResizeImage(IplImage * img);
+public:
+	afx_msg void OnBnClickedGo();
 };
+
+
+
+//class CPanel: public CStatic
+//{
+//	BOOL OnCmdMsg(UINT nID, int nCode, void* pExtra, AFX_CMDHANDLERINFO* pHandlerInfo){
+//		if (CStatic::OnCmdMsg(nID, nCode, pExtra, pHandlerInfo))
+//			return TRUE;
+//		CWnd* pParent = GetParent();
+//		if (pParent != NULL)
+//			return pParent->OnCmdMsg(nID, nCode, pExtra, pHandlerInfo);
+//		return FALSE;
+//	}
+//};
