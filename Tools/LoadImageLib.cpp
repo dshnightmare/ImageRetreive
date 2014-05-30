23b4d58f9491b34ef318ad33164e334e3fd00ac7
dengsh <dshnightmare@gmail.com>
2014-05-30 14:15:55 +0800

    读入图片后加入resize

--- Tools/LoadImageLib.cpp
+++ Tools/LoadImageLib.cpp
@@ -19,7 +19,9 @@ DLLEXPORT  MyMat* LoadFromCIFAR10(string path)
 	int imgSize=width*height;
 	int nChannels=3;
 	int imgDataSize = 1+imgSize*nChannels;
-
+	Mat kernel = (Mat_<float>(3, 3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
+	Mat img;
+	Mat rs;
 	for(int m = 0; m < 5; m++)
 	{
 		int n = 0;
@@ -29,19 +31,25 @@ DLLEXPORT  MyMat* LoadFromCIFAR10(string path)
 		{
 			if(n >= 10000)
 				break;
+
+			if(n >= 10000)
+				break;
+			img.create(width, height, type);
+			rs.create(64, 64, type);
 			imgs[m * 10000 + n].create(width, height, type);
 			uchar *buf=(uchar*)calloc(imgDataSize,sizeof(uchar));
 			fin.read((char *)buf,(imgDataSize)*sizeof(uchar));
-			imgs[m * 10000 + n].type = buf[0];
-			imgs[m * 10000 + n].id = m * 10000 + n;
 			for(int i = 0; i < height; i++) 
 			{
 				for(int j = 0; j < width; j++ ) {
-					uchar* dataIJ = imgs[m * 10000 + n].data + i * imgs[m * 10000 + n].step + j * imgs[m * 10000 + n].elemSize();// img.at(i, j)
+					uchar* dataIJ = img.data + i * img.step + j * img.elemSize();// img.at(i, j)
 					for(int k = 0; k < nChannels; k++)
 						dataIJ[k] = buf[1 + planeId[k] * imgSize + i * width + j];
 				}
 			}
+			//拉普拉斯锐化
+			resize(img, rs, rs.size(), 0, 0, INTER_CUBIC);
+			filter2D(rs, imgs[m * 10000 + n], rs.depth(), kernel );
 			n++;
 			free(buf);
 		}
